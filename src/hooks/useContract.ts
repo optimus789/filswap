c75792ad19e116a2f112b3c480a2421ca1ea570e
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2023-01-28 03:24:00 +0530

    Added Hyperspace

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -77,6 +77,7 @@ export function useENSRegistrarContract(withSignerIfPossible?: boolean): Contrac
       case ChainId.MAINNET:
       case ChainId.GÖRLI:
       case ChainId.ROPSTEN:
+      case ChainId.HYPERSPACE_TESTNET:
       case ChainId.RINKEBY:
         address = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
         break
